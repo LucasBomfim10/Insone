b631f3717c9422a9475833bfb3b39220fb2be1c7
Emanuelly <onlyfstudy@gmail.com>
2023-05-31 14:05:14 -0300

    sprite
    mudei o sprite da personagem

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ var livesText; // variável global para o texto de vidas
 function preload() {
 
     // Carregando a spritesheet do player
-    this.load.spritesheet('player', 'assets/Manu (1).png', {
-        frameWidth: 157,
-        frameHeight: 157
+    this.load.spritesheet('player', 'assets/ella1 (1).png', {
+        frameWidth: 200,
+        frameHeight: 200
     });
     // Carregando as imagens
     this.load.image('map', 'assets/inicio-jogo-atualizado.png');
@@ -69,23 +69,23 @@ function create() {
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 7, end: 12 }),
 
-        frameRate: 10,
+        frameRate: 12,
         repeat: -1
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
+        frames: [{ key: 'player', frame: 0 }],
         frameRate: 20
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 1, end: 6 }),
         //frames: [{ key: 'player', frame: 2 }],
-        frameRate: 10,
+        frameRate: 12,
         repeat: -1
     });
 
