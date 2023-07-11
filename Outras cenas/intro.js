3500cf138f1144a9299670b5eddbb220fd61d73e
LUCAS BOMFIM <lucadez01@gmail.com>
2023-07-11 16:17:23 -0300

    menuV1

--- Outras cenas/intro.js
+++ Outras cenas/intro.js
@@ -1,22 +1,85 @@
 var introConfig = {
-    key: 'intro',
-    preload: function () {
-        // Carregando as imagens de introdução
-        this.load.image('intro1', 'assets/Intro1.png');
-        this.load.image('intro2', 'assets/Intro2.png');
-        this.load.image('intro3', 'assets/Intro3.png');
-    },
-    create: function () {
-        // Adicionando as imagens de introdução
-        this.add.image(400, 300, 'intro1').setScale(1);
-        setTimeout(() => {
-            this.add.image(400, 300, 'intro2').setScale(1);
-        }, 500); // exibir a segunda imagem após 5 segundos
-        setTimeout(() => {
-            this.add.image(400, 300, 'intro3').setScale(1);
-        }, 1000); // exibir a terceira imagem após 10 segundos
-        setTimeout(() => {
-            this.scene.start('main'); // carregar a cena principal após 15 segundos
-        }, 1500);
+  key: "intro",
+  preload: function () {},
+  create: function () {
+    var self = this; // Armazena uma referência ao contexto atual
+
+    // Configurações do texto
+    var textConfig = {
+      fontFamily: "Arial",
+      fontSize: "24px",
+      color: "#ffffff",
+      align: "center",
+      wordWrap: { width: 600, useAdvancedWrap: true },
+      stroke: "#000000",
+      strokeThickness: 4,
+      shadow: {
+        offsetX: 2,
+        offsetY: 2,
+        color: "#000000",
+        blur: 2,
+        stroke: true,
+        fill: true,
+      },
+    };
+
+    // Função para limpar a tela
+    function clearScreen() {
+      self.children.removeAll();
     }
+
+    // Adicionando o texto da introdução - Imagem "Intro1"
+    var introText1 =
+      "Está a noite e uma garota se afasta da vila com um balde e um tridente em sua mão. Ela senta próxima ao rio e deita na grama. Vemos a noite virar dia e a garota ainda está acordada contemplando o céu vazio.";
+
+    clearScreen();
+
+    self.add
+      .text(
+        game.config.width / 2,
+        game.config.height / 2,
+        introText1,
+        textConfig
+      )
+      .setOrigin(0.5);
+
+    setTimeout(() => {
+      clearScreen();
+
+      // Adicionando o texto da introdução - Imagem "Intro2"
+      var introText2 =
+        "De dia, ela escuta um som gemido macabro vindo da vila. Ao se aproximar do seu vilarejo, ela vê que não há mais ninguém presente. Procurando um pouco mais, ela vê todos os moradores mortos empilhados atrás de uma casa. A garota não sabe como reagir, mas vê que há um caminho que não existia antes ali. Esse caminho a leva a uma árvore que, por algum motivo, a atrai. Ao se aproximar, surgem vários corpos enforcados nos galhos da árvore e vários demônios aparecem ao seu redor. A personagem entra em pânico com os seres se aproximando, mas ela é levitada por alguma coisa e é marcada com estigmas em suas duas mãos.";
+
+      self.add
+        .text(
+          game.config.width / 2,
+          game.config.height / 2,
+          introText2,
+          textConfig
+        )
+        .setOrigin(0.5);
+
+      setTimeout(() => {
+        clearScreen();
+
+        // Adicionando o texto da introdução - Imagem "Intro3"
+        var introText3 =
+          "Parece que há um som que faz os demônios andarem em uma certa direção, e a personagem vê sua oportunidade de correr. Após várias horas correndo, ela se depara com a floresta que antes visitara, mas agora com um ar esbranquiçado e sem vida.";
+
+        self.add
+          .text(
+            game.config.width / 2,
+            game.config.height / 2,
+            introText3,
+            textConfig
+          )
+          .setOrigin(0.5);
+
+        setTimeout(() => {
+          self.scene.stop("intro");
+          game.scene.start("cena1"); // carregar a cena principal após o tempo desejado
+        }, 8000); // tempo de exibição da tela preta e do texto em milissegundos
+      }, 15000); // tempo de exibição da Imagem "Intro2"
+    }, 5000); // tempo de exibição da Imagem "Intro1"
+  },
 };
